3895f68f887572a4e309b12575e58cd007af74b6
jozsadavid-create <jozsadavid@hotmail.de>
2026-09-20 10:28:56 +0200

    fix

--- data/nolka.js
+++ data/nolka.js
@@ -28,7 +28,7 @@ var NOLKA_DATA = {
       "Lilac_Roller_at_the_Top.jpg",
       "Cape_Vulture.jpg",
       "MoonYawn_Open_Pouch.jpg",
-      "DangerousFlatmate_Kruger",
+      "DangerousFlatmate_Kruger.jpg",
       "Herd_at_the_Riverbed.jpg",
       "Young_Elephant_Scrub.jpg",
       "Wildebeest_BW.jpg",
